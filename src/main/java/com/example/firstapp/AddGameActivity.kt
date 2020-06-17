d962e5763f64bb2174afc1ddcd6ac1b4735a6cd2
Servebotfrank <servebotfrank@gmail.com>
2020-06-16 19:11:36 -0800

    First commit

---
+++ src/main/java/com/example/firstapp/AddGameActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddGameActivity: AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_game)
+    }
+
+}
